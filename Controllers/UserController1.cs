c64e705a7038a9ffd073c3349d7307c3f9d48c5b
FrancoisFourie33 <louisfrancois2002@icloud.com>
2024-04-01 22:51:19 +0200

    Part1_secondCommit

--- Controllers/UserController1.cs
+++ Controllers/UserController1.cs
@@ -1,13 +1,25 @@
 ﻿using CLDV6211_POE_Part1.Models;
 using Microsoft.AspNetCore.Mvc;
+using System.Data.SqlClient;
 
 namespace CLDV6211_POE_Part1.Controllers
 {
     public class UserController1 : Controller
     {
+
+        private static string con_string = "Server=tcp:cldv-sql-server-st10152431.database.windows.net,1433;Initial Catalog=CLDV_DB1;Persist Security Info=False;User ID=LouisFrancoisFourie;Password=Monkeys2002;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;";
+        private static SqlConnection con = new SqlConnection(con_string);
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+
+
+
         public Table_1 Table_1 = new Table_1();
 
-        [HttpPost]
 
         public ActionResult About(Table_1 Users)
         {
@@ -15,10 +27,6 @@ namespace CLDV6211_POE_Part1.Controllers
             return RedirectToAction("Home", "About");
         }
 
-        [HttpGet]
-        public IActionResult Index() 
-        {
-            return View();
-        }
     }
+
 }
